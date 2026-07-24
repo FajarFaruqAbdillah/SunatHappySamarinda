b16048bea86cca48c454e3c6af29d2f46d417ed0
Fajar <faruqabdillah11@gmail.com>
2026-07-24 09:12:05 +0800

    Add responsive photo gallery

---
+++ src/data/gallery.ts
@@ -0,0 +1,73 @@
+export interface GalleryImage {
+  src: string;
+  alt: string;
+  caption: string;
+  width: number;
+  height: number;
+}
+
+export const galleryImages: GalleryImage[] = [
+  {
+    src: "/images/gallery-3.jpg",
+    alt: "Tim Rumah Sunat Happy Samarinda berfoto bersama",
+    caption: "Tim Rumah Sunat Happy Samarinda",
+    width: 1600,
+    height: 1200,
+  },
+  {
+    src: "/images/team-smile.jpg",
+    alt: "Tim Rumah Sunat Happy Samarinda tersenyum bersama",
+    caption: "Tim yang ramah dan siap mendampingi keluarga",
+    width: 1600,
+    height: 1200,
+  },
+  {
+    src: "/images/gallery-1.jpg",
+    alt: "Banner layanan Sunat Happy Samarinda berwarna biru dan hijau",
+    caption: "Sunat Happy: aman, nyaman, dan profesional",
+    width: 1402,
+    height: 1122,
+  },
+  {
+    src: "/images/documentation-01.jpg",
+    alt: "Dokumentasi pasien bersama pendamping Rumah Sunat Happy",
+    caption: "Momen nyaman bersama pasien",
+    width: 900,
+    height: 1600,
+  },
+  {
+    src: "/images/documentation-02.jpg",
+    alt: "Dokumentasi pasien anak bersama tim Rumah Sunat Happy",
+    caption: "Pendampingan yang hangat dan ramah",
+    width: 900,
+    height: 1600,
+  },
+  {
+    src: "/images/documentation-03.jpg",
+    alt: "Pasien anak tersenyum bersama tim Rumah Sunat Happy",
+    caption: "Senyum pasien adalah kebahagiaan kami",
+    width: 900,
+    height: 1600,
+  },
+  {
+    src: "/images/documentation-04.jpg",
+    alt: "Dokumentasi layanan pasien anak Rumah Sunat Happy",
+    caption: "Layanan nyaman untuk anak",
+    width: 900,
+    height: 1600,
+  },
+  {
+    src: "/images/documentation-05.jpg",
+    alt: "Pasien anak memberi tanda jempol bersama tim Sunat Happy",
+    caption: "Anak tetap tenang dan percaya diri",
+    width: 900,
+    height: 1600,
+  },
+  {
+    src: "/images/gallery-2.jpg",
+    alt: "Tim dalam workshop sunat modern di Samarinda",
+    caption: "Workshop dan pengembangan kompetensi tim",
+    width: 800,
+    height: 600,
+  },
+];
